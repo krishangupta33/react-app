30598680b13ad235fe6285bf178d914411d269d9
krishangupta33 <k.gupta035@gmail.com>
2024-03-19 10:37:52 -0400

    message app

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//PascalCase    
+function Message() {
+    //JSX: Javascript XML
+    const name = "Krishan";
+    if(name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello World</h1>;
+}
+
+export default Message;
