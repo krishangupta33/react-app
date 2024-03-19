d2ca8f7cbf3506a61690d0704b2d6920d7d671f7
krishangupta33 <k.gupta035@gmail.com>
2024-03-19 12:26:55 -0400

    state props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message /></div>
-  
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
